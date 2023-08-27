738276b8d581ac9cb03d5096a50c2ea3cd529e8a
Deepak Ramani <dr563105@gmail.com>
2023-08-28 02:01:39 +0530

    fixed datagen python script

--- generate-data/user_product_data.py
+++ generate-data/user_product_data.py
@@ -3,49 +3,59 @@ import argparse
 from time import sleep
 import random
 from faker import Faker
+import os
 
 Faker.seed(42)
 fake = Faker()
 
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "cdc-demo-db")
+POSTGRES_HOSTNAME = os.getenv("POSTGRES_HOST", "postgres")
+
+
 def gen_user_product_data(num_records: int) -> None:
+    # connection = (f"dbname={POSTGRES_DB} user={POSTGRES_USER} password={POSTGRES_PASSWORD} host={POSTGRES_HOSTNAME}")
     for id in range(num_records):
-        sleep(0.5)
-        conn = psycopg2.connect(
-            "dbname='postgres' user='postgres' host='postgres' password='postgres'"
-        )
+        id +=1
+        # sleep(0.5)
+        conn = psycopg2.connect(database=f'{POSTGRES_DB}',
+                                user=f'{POSTGRES_USER}',
+                                password=f'{POSTGRES_PASSWORD}',
+                                host=f'{POSTGRES_HOSTNAME}')
         curr = conn.cursor()
         curr.execute(
             f"INSERT INTO \
                 commerce.users (id, username, email_address) \
-                VALUES ({id}, {fake.user_name()}, {fake.email()})"
+                VALUES (%s, %s, %s)", (id, fake.user_name(), fake.email())
         )
         curr.execute(
             f"INSERT INTO \
                 commerce.products (id, name, description, price) \
-                VALUES ({id}, {fake.name()}, {fake.text()}, {fake.random_int(min=1, max=100)})"
+                VALUES (%s, %s, %s, %s)", (id, fake.name(), fake.text(), (fake.random_int(min=1, max=999))/100)
         )
         conn.commit()
 
-        sleep(0.5)
+        # sleep(0.5)
         # update 10 % of the time
         if random.randint(1, 100) >= 90:
             curr.execute(
-                f"UPDATE commerce.users \
-                    SET username = {fake.user_name()} \
-                    WHERE id = {id}"
+                "UPDATE commerce.users \
+                    SET username = %s\
+                    WHERE id = %s",(fake.user_name(), id)
             )
             curr.execute(
-                f"UPDATE commerce.products \
-                    SET name = {fake.name()} \
-                    WHERE id = {id}"
+                "UPDATE commerce.products \
+                    SET name = %s \
+                    WHERE id = %s",(fake.user_name(), id)
             )
         conn.commit()
 
-        sleep(0.5)
+        # sleep(0.5)
         # delete 5 % of the time
         if random.randint(1, 100) >= 95:
-            curr.execute(f"DELETE FROM commerce.users WHERE id = {id}")
-            curr.execute(f"DELETE FROM commerce.products WHERE id = {id}")
+            curr.execute("DELETE FROM commerce.users WHERE id = %s",(id,))
+            curr.execute("DELETE FROM commerce.products WHERE id = %s", (id,))
 
         conn.commit()
         curr.close()
@@ -60,7 +70,7 @@ if __name__ == "__main__":
         "--num_records",
         type=int,
         help="Number of records to generate",
-        default=1000,
+        default=100_000,
     )
     args = parser.parse_args()
     num_records = args.num_records
--- postgres/init.sql
+++ postgres/init.sql
@@ -6,14 +6,13 @@ SET search_path TO commerce;
 
 -- create a users table
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
+    id int PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
-    email_address TEXT NOT NULL UNIQUE,
-    CONSTRAINT check_email CHECK (email_address ~* '^[A-Za-z0-9]+@[A-Za-z0-9]+.[A-Za-z0-9]+$')
+    email_address TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS products (
-   id SERIAL PRIMARY KEY, 
+   id int PRIMARY KEY, 
    name VARCHAR(255) NOT NULL,
    description TEXT,
    price NUMERIC(5,2) NOT NULL
